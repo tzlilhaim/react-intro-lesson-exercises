0ba099f074d96de54a5d6d034cdebf89ff44c0ec
tzlilhaim <tzlilhaim480@gmail.com>
2020-09-29 18:41:36 +0300

    Added solutions

--- src/App.js
+++ src/App.js
@@ -1,65 +1,86 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
 
 class App extends Component {
   getStuff() {
+    return <h1>This is a header</h1>
   }
 
   getMorningGreeting() {
+    return <div>Good morning!</div>
   }
 
   getEveningGreeting() {
+    return <div>Good evening!</div>
   }
 
   showCompany(name, revenue) {
+    return (
+      <div id={name}>
+        {name} makes {revenue} every year
+      </div>
+    )
   }
 
   getClassName(temperature) {
-
+    const weather =
+      temperature < 15
+        ? "freezing"
+        : temperature > 14 && temperature < 31
+        ? "fair"
+        : "hell-scape"
+    return <div id="weatherBox" className={weather}></div>
   }
 
   render() {
-
     let companies = [
       { name: "Tesla", revenue: 140 },
       { name: "Microsoft", revenue: 300 },
-      { name: "Google", revenue: 600 }
+      { name: "Google", revenue: 600 },
     ]
 
     return (
       <div>
         <div className="ex-space">
-          <h4 className='ex-title'>Spot-check 1</h4 >
+          <h4 className="ex-title">Spot-check 1</h4>
           <div className="exercise" id="spotcheck-1">
-            {/* your code here */}
+            {this.getStuff()}
           </div>
         </div>
 
         <div className="ex-space">
-          <h4 className='ex-title'>Spot-check 2</h4>
+          <h4 className="ex-title">Spot-check 2</h4>
           <div className="exercise" id="spotcheck-2">
-            {/* your code here */}
+            {new Date().getHours() < 12
+              ? this.getMorningGreeting()
+              : this.getEveningGreeting()}
           </div>
         </div>
 
         <div className="ex-space">
-          <h4 className='ex-title'>Spot-check 3</h4>
+          <h4 className="ex-title">Spot-check 3</h4>
           <div className="exercise" id="spotcheck-3">
-            {/* your code here */}
+            {[
+              this.getMorningGreeting(),
+              this.getEveningGreeting(),
+              <p>some text</p>,
+            ]}
           </div>
         </div>
 
         <div className="ex-space">
-          <h4 className='ex-title'>Exercise 1</h4>
+          <h4 className="ex-title">Exercise 1</h4>
           <div className="exercise" id="ex-1">
-            {/* your code here */}
+            {companies.map((c) => {
+              return this.showCompany(c.name, c.revenue)
+            })}
           </div>
         </div>
 
         <div className="ex-space">
-          <h4 className='ex-title'>Exercise 2</h4>
+          <h4 className="ex-title">Exercise 2</h4>
           <div className="exercise" id="ex-2">
-            {/* your code here */}
+            {this.getClassName(30)}
           </div>
         </div>
       </div>
@@ -67,4 +88,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
